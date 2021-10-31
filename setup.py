8396491982a91d5f1e308783b17170c26ad7cc4e
Ambistic <nathan_v@hotmail.fr>
2021-10-31 12:09:06 +0100

    adding dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="jf-Ambistic",
+    name="jf",
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
@@ -23,4 +23,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=['pandas', 'networkx'],
 )
